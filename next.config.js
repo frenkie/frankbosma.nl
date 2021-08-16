43511354ed515cf1f8a2f2737ad936469dc82d61
frank <frank@frankbosma.nl>
2021-08-16 16:22:31 +0200

    Added timeline details, links and other updates to specific projects

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
         // for fetching static data / images instead of Strapi content
         // Prerequisite is that images and data (as referenced in the below file) are available in the /public folder
 
-        NEXT_PUBLIC_DATA_PATH: '/data/20210520-home.json', // will be fetched client side
-        NEXT_PUBLIC_DATA_PATH_RESUME: '/data/20210520-resume.json' // will be fetched client side
+        NEXT_PUBLIC_DATA_PATH: '/data/20210816-home.json', // will be fetched client side
+        NEXT_PUBLIC_DATA_PATH_RESUME: '/data/20210816-resume.json' // will be fetched client side
     }
 };
