9e41d2f4a4507bb9f8f91bc650e5216bb85e3ff0
frank <frank@frankbosma.nl>
2023-01-26 16:28:02 +0100

    added new images for Spacebeest

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
         // for fetching static data / images instead of Strapi content
         // Prerequisite is that images and data (as referenced in the below file) are available in the /public folder
 
-        NEXT_PUBLIC_DATA_PATH: '/data/20220426-home.json', // will be fetched client side
-        NEXT_PUBLIC_DATA_PATH_RESUME: '/data/20220426-resume.json' // will be fetched client side
+        NEXT_PUBLIC_DATA_PATH: '/data/20230126-home.json', // will be fetched client side
+        NEXT_PUBLIC_DATA_PATH_RESUME: '/data/20230126-resume.json' // will be fetched client side
     }
 };
